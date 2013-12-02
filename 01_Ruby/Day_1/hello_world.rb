9d323606a63a352cc42599528b12c569946ce090
Adam Walz <adam@adamwalz.net>
2013-12-01 21:32:55 -0700

    Adding some more ways to iterate in Ruby

--- 01_Ruby/Day_1/hello_world.rb
+++ 01_Ruby/Day_1/hello_world.rb
@@ -1,8 +1,8 @@
 #----------------------------------------------------------------------------
 #          FILE:  hello_world.rb
-#   DESCRIPTION:  Seven Languages in Seven Weeks - Ruby - Day 1 Excercises
+#   DESCRIPTION:  Seven Languages in Seven Weeks - Ruby - Day 1 Exercises
 #        AUTHOR:  Adam Walz <adam@adamwalz.net>
-#       VERSION:  1.0.0
+#       VERSION:  1.0.1
 #----------------------------------------------------------------------------
 
 # Print the string "Hello, World"
@@ -13,8 +13,16 @@ puts 'Hello, Ruby'.index 'Ruby'
 
 # Print your name 10 times
 for i in 0..9 do puts 'Adam Walz' end
+# Strings can also be 'multiplied' to get repetitive string concatenation
+puts 'Adam Walz ' * 10
+# Or using blocks
+10.times { puts "Adam Walz" }
 
 # Print the string "This is sentence number 1" where the 1 changes from 1 to 10
 for i in 1..10 do
   puts "This is sentence number #{i}"
 end
+
+# Or using blocks
+(1..10).each { |i| puts "This is sentence number #{i}" }
+1.upto(10) { |i| puts "This is sentence number #{i}" }
